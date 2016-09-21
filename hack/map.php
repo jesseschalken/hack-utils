245c0895408b9d00f8f313777889e3ea53948ca8
Jesse Schalken <me@jesseschalken.com>
2016-09-22 00:45:24 +1000

    Make map\*() functions work well with array<int, T>
    Originally I wanted to force map arrays to be array<arraykey, T> because array<string, T> is lies (PHP converts intish string keys to ints), but this meant an array<int, T> couldn't be used. So just allow any array key and let the user ensure they're not using an array<string, T>.

--- hack/map.php
+++ hack/map.php
@@ -9,7 +9,7 @@ use function HackUtils\new_null;
 
 type key = arraykey;
 
-function to_pairs<T>(map<T> $map): array<(key, T)> {
+function to_pairs<Tk, Tv>(array<Tk, Tv> $map): array<(Tk, Tv)> {
   $r = [];
   foreach ($map as $k => $v) {
     $r[] = tuple($k, $v);
@@ -17,7 +17,7 @@ function to_pairs<T>(map<T> $map): array<(key, T)> {
   return $r;
 }
 
-function from_pairs<T>(array<(key, T)> $pairs): map<T> {
+function from_pairs<Tk, Tv>(array<(Tk, Tv)> $pairs): array<Tk, Tv> {
   $r = [];
   foreach ($pairs as $p) {
     $r[$p[0]] = $p[1];
@@ -25,15 +25,15 @@ function from_pairs<T>(array<(key, T)> $pairs): map<T> {
   return $r;
 }
 
-function chunk<T>(map<T> $map, int $size): array<map<T>> {
+function chunk<Tk, Tv>(array<Tk, Tv> $map, int $size): array<array<Tk, Tv>> {
   return \array_chunk($map, $size, true);
 }
 
-function soft_get<T>(map<T> $map, key $key): ?T {
+function soft_get<Tk, Tv>(array<Tk, Tv> $map, Tk $key): ?Tv {
   return $map[$key] ?? new_null();
 }
 
-function get_default<T>(map<T> $map, key $key, T $default): T {
+function get_default<Tk, Tv>(array<Tk, Tv> $map, Tk $key, Tv $default): Tv {
   return has_key($map, $key) ? $map[$key] : $default;
 }
 
@@ -49,15 +49,15 @@ function fixkeys(array<key> $keys): array<string> {
   return vector\map($keys, $key ==> (string) $key);
 }
 
-function column<T>(array<map<T>> $maps, key $key): array<T> {
+function column<Tk, Tv>(array<array<Tk, Tv>> $maps, Tk $key): array<Tv> {
   return \array_column($maps, $key);
 }
 
-function combine<T>(array<key> $keys, array<T> $values): map<T> {
+function combine<Tk, Tv>(array<Tk> $keys, array<Tv> $values): array<Tk, Tv> {
   return \array_combine($keys, $values);
 }
 
-function separate<T>(map<T> $map): (array<key>, array<T>) {
+function separate<Tk, Tv>(array<Tk, Tv> $map): (array<Tk>, array<Tv>) {
   $ks = [];
   $vs = [];
   foreach ($map as $k => $v) {
@@ -67,48 +67,48 @@ function separate<T>(map<T> $map): (array<key>, array<T>) {
   return tuple($ks, $vs);
 }
 
-function fill_keys<T>(array<key> $keys, T $value): map<T> {
+function fill_keys<Tk, Tv>(array<Tk> $keys, Tv $value): array<Tk, Tv> {
   return \array_fill_keys($keys, $value);
 }
 
-function flip(map<key> $map): map<key> {
+function flip<Tk, Tv as key>(array<Tk, Tv> $map): array<Tv, Tk> {
   return \array_flip($map);
 }
 
-function has_key(map<mixed> $map, key $key): bool {
+function has_key<Tk>(array<Tk, mixed> $map, Tk $key): bool {
   return \array_key_exists($key, $map);
 }
 
-function keys(map<mixed> $map): array<key> {
+function keys<Tk>(array<Tk, mixed> $map): array<Tk> {
   return \array_keys($map);
 }
 
-function values<T>(map<T> $map): array<T> {
+function values<Tv>(map<Tv> $map): array<Tv> {
   return \array_values($map);
 }
 
-function value_keys<T>(map<T> $map, T $value): array<key> {
+function value_keys<Tk, Tv>(array<Tk, Tv> $map, Tv $value): array<Tk> {
   return \array_keys($map, $value, true);
 }
 
-function union<T>(map<T> $a, map<T> $b): map<T> {
+function union<Tk, Tv>(array<Tk, Tv> $a, array<Tk, Tv> $b): array<Tk, Tv> {
   return \array_replace($a, $b);
 }
 
-function union_all<T>(array<map<T>> $maps): map<T> {
+function union_all<Tk, Tv>(array<array<Tk, Tv>> $maps): array<Tk, Tv> {
   return \call_user_func_array('array_replace', $maps);
 }
 
-function reverse<T>(map<T> $map): map<T> {
+function reverse<Tk, Tv>(array<Tk, Tv> $map): array<Tk, Tv> {
   return \array_reverse($map, true);
 }
 
-function find<T>(map<T> $map, T $value): ?key {
+function find<Tk, Tv>(array<Tk, Tv> $map, Tv $value): ?Tk {
   $ret = \array_search($map, $value, true);
   return $ret === false ? null : $ret;
 }
 
-function slice<T>(map<T> $map, int $offset, ?int $length = null): map<T> {
+function slice<Tk, Tv>(array<Tk, Tv> $map, int $offset, ?int $length = null): array<Tk, Tv> {
   return \array_slice($map, $offset, $length, true);
 }
 
@@ -120,10 +120,10 @@ function contains<T>(map<T> $map, T $value): bool {
   return \in_array($value, $map, true);
 }
 
-function sort_keys<T>(
-  map<T> $map,
-  ?(function(key, key): int) $cmp = null,
-): map<T> {
+function sort_keys<Tk, Tv>(
+  array<Tk, Tv> $map,
+  ?(function(Tk, Tk): int) $cmp = null,
+): array<Tk, Tv> {
   if ($cmp !== null) {
     \uksort($map, $cmp);
   } else {
@@ -132,16 +132,16 @@ function sort_keys<T>(
   return $map;
 }
 
-function sort<T>(map<T> $map, (function(T, T): int) $cmp): map<T> {
+function sort<Tk, Tv>(array<Tk, Tv> $map, (function(Tv, Tv): int) $cmp): array<Tk, Tv> {
   \uasort($map, $cmp);
   return $map;
 }
 
-function filter<T>(map<T> $map, (function(T): bool) $f): map<T> {
+function filter<Tk, Tv>(array<Tk, Tv> $map, (function(Tv): bool) $f): array<Tk, Tv> {
   return \array_filter($map, $f);
 }
 
-function map<Tin, Tout>(map<Tin> $map, (function(Tin): Tout) $f): map<Tout> {
+function map<Tk, Tin, Tout>(array<Tk, Tin> $map, (function(Tin): Tout) $f): array<Tk, Tout> {
   return \array_map($f, $map);
 }
 
@@ -156,11 +156,11 @@ function reduce<Tin, Tout>(
 /**
  * Extract multiple keys from a map at once.
  */
-function select<T>(map<T> $map, array<key> $keys): array<T> {
+function select<Tk, Tv>(array<Tk, Tv> $map, array<Tk> $keys): array<Tv> {
   return vector\map($keys, $key ==> $map[$key]);
 }
 
-function zip<Ta, Tb>(map<Ta> $a, map<Tb> $b): map<(Ta, Tb)> {
+function zip<Tk, Ta, Tb>(array<Tk, Ta> $a, array<Tk, Tb> $b): array<Tk, (Ta, Tb)> {
   $ret = [];
   foreach ($a as $k => $v) {
     if (has_key($b, $k)) {
@@ -170,7 +170,7 @@ function zip<Ta, Tb>(map<Ta> $a, map<Tb> $b): map<(Ta, Tb)> {
   return $ret;
 }
 
-function unzip<Ta, Tb>(map<(Ta, Tb)> $map): (map<Ta>, map<Tb>) {
+function unzip<Tk, Ta, Tb>(array<Tk, (Ta, Tb)> $map): (array<Tk, Ta>, array<Tk, Tb>) {
   $a = [];
   $b = [];
   foreach ($map as $k => $v) {
