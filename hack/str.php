6d745cddce4c3297f944f07d1c3ccd824355ce4d
Jesse Schalken <me@jesseschalken.com>
2016-10-04 19:34:23 +1100

    Add str\{encode,decode}_{list,map}() functions

--- hack/str.php
+++ hack/str.php
@@ -53,16 +53,94 @@ function trim_right(string $string, string $chars = TRIM_CHARS): string {
   return \rtrim($string, $chars);
 }
 
-function escape(string $string, string $chars): string {
-  if ($string === '')
-    return $string;
-  $string = pair\fst(replace($string, '\\', '\\\\'));
-  $length = length($chars);
-  for ($i = 0; $i < $length; $i++) {
-    $char = $chars[$i];
-    $string = pair\fst(replace($string, $char, '\\'.$char));
+/**
+ * Prefixes characters with a backslash, including backslashes.
+ */
+function escape_chars(string $s, string $chars): string {
+  if ($s === '')
+    return $s;
+  $s = replace($s, '\\', '\\\\');
+  $l = length($chars);
+  for ($i = 0; $i < $l; $i++) {
+    $c = $chars[$i];
+    $s = replace($s, $c, '\\'.$c);
   }
-  return $string;
+  return $s;
+}
+
+function encode_list(array<string> $list): string {
+  $r = '';
+  foreach ($list as $x) {
+    $r .= escape_chars($x, ';').';';
+  }
+  return $r;
+}
+
+function decode_list(string $s): array<string> {
+  $r = [];
+  $b = '';
+  $e = false;
+  $l = \strlen($s);
+  for ($i = 0; $i < $l; $i++) {
+    $c = $s[$i];
+    if ($e) {
+      $b .= $c;
+      $e = false;
+    } else if ($c === '\\') {
+      $e = true;
+    } else if ($c === ';') {
+      $r[] = $b;
+      $b = '';
+    } else {
+      $b .= $c;
+    }
+  }
+  return $r;
+}
+
+function encode_map(array<arraykey, string> $map): string {
+  $r = '';
+  foreach ($map as $k => $v) {
+    $k .= '';
+    $r .= escape_chars($k, '=;').'=';
+    $r .= escape_chars($v, '=;').';';
+  }
+  return $r;
+}
+
+function decode_map(string $s): array<arraykey, string> {
+  $r = [];
+  $k = null;
+  $b = '';
+  $l = \strlen($s);
+  $e = false;
+  for ($i = 0; $i < $l; $i++) {
+    $c = $s[$i];
+    if ($e) {
+      $b .= $c;
+      $e = false;
+    } else if ($c === '\\') {
+      $e = true;
+    } else if ($c === '=') {
+      // Make sure we are expecting a key
+      if ($k !== null) {
+        throw new \Exception('Double key');
+      }
+      $k = $b;
+      $b = '';
+    } else if ($c === ';') {
+      // Make sure we are expecting a value
+      if ($k === null) {
+        throw new \Exception('Value without key');
+      }
+      $r[$k] = $b;
+      $k = null;
+      $b = '';
+    } else {
+      $b .= $c;
+    }
+  }
+  return $r;
 }
 
 function split(
@@ -197,6 +275,23 @@ function join(array<string> $strings, string $delimiter = ''): string {
 }
 
 function replace(
+  string $subject,
+  string $search,
+  string $replace,
+  bool $caseInsensitive = false,
+): string {
+  $count = 0;
+  $result =
+    $caseInsensitive
+      ? \str_ireplace($search, $replace, $subject)
+      : \str_replace($search, $replace, $subject);
+  if (!\is_string($result)) {
+    throw new \Exception('str_i?replace() failed');
+  }
+  return $result;
+}
+
+function replace_count(
   string $subject,
   string $search,
   string $replace,
