94a135464b57edd3956a22a02e9f01649af16652
Jesse Schalken <me@jesseschalken.com>
2016-10-26 06:30:48 +1100

    Fix include

--- php/include.php
+++ php/include.php
@@ -5,7 +5,6 @@ require_once (__DIR__."/pcre.php");
 require_once (__DIR__."/math.php");
 require_once (__DIR__."/ctype.php");
 require_once (__DIR__."/DateTime.php");
-require_once (__DIR__."/encode.php");
 require_once (__DIR__."/json.php");
 require_once (__DIR__."/sort.php");
 require_once (__DIR__."/test.php");
