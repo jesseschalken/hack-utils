b3e2c9561dc351a2cbe15a42b4ff1447ff95aa1e
Jesse Schalken <me@jesseschalken.com>
2016-11-15 12:58:32 +1100

    Add missing include for ArrayIterator file

--- hack/include.php
+++ hack/include.php
@@ -8,3 +8,4 @@ require_once __DIR__.'/DateTime.php';
 require_once __DIR__.'/json.php';
 require_once __DIR__.'/sort.php';
 require_once __DIR__.'/test.php';
+require_once __DIR__.'/ArrayIterator.php';
