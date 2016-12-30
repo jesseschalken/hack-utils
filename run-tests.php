1863f4e2b21589b88bb462dd3fa9e6824934c73b
Jesse Schalken <me@jesseschalken.com>
2016-12-30 19:39:51 +1100

    Try to generate test coverage

--- run-tests.php
+++ run-tests.php
@@ -5,4 +5,18 @@ namespace HackUtils;
 
 require_once __DIR__ . '/vendor/autoload.php';
 
-_run_tests();
+$coverage = new \SebastianBergmann\CodeCoverage\CodeCoverage();
+$coverage->setAddUncoveredFilesFromWhitelist(true);
+$coverage->filter()->addDirectoryToWhitelist(__DIR__ . '/php');
+$coverage->start('Hack Utils');
+
+try {
+    _run_tests();
+} finally {
+    $coverage->stop();
+
+    $writer = new \SebastianBergmann\CodeCoverage\Report\Html\Facade();
+    $writer->process($coverage, __DIR__ . '/coverage');
+}
+
+
